c9252227c3d1e18646bb4f8dfe272354c1b599ab
Petr Barabáš <petr.barabas@memos.cz>
2023-09-13 14:51:22 +0200

    Navbar init

--- SinatraWebsite/src/components/Hero.tsx
+++ SinatraWebsite/src/components/Hero.tsx
@@ -1,36 +1,78 @@
 import Frank from '../assets/frank.jpg';
 
-
-
-
-
 export default function Hero() {
+
+
   return (
 
-    <div className="flex flex-row w-full h-screen">
+    <section className="flex flex-row w-full h-screen">
+      
         {/* left side with text*/ }
         <div className=" w-full h-screen flex justify-center items-center">
         
-         {/* container for texts*/ }
-         <div className="flex flex-col gap-4">
+         {/* container for texts
+         
+         mobily - nothing - 40px
+                  the best - 64px
+                  podpis - 48
 
-            <div className="font-bebas  flex flex-row justify-end items-end gap-4">
-              <h2 className="text-font-color text-7xl">nothing but 
+                  
+         
+         */ }
+         <div className="
+         flex 
+         flex-col 
+         md:gap-6
+         ">
+
+            <div className="font-bebas  
+            md:flex 
+            md:flex-row 
+            md:justify-end 
+            md:items-end 
+            md:gap-4
+            
+            
+
+
+            ">
+              <h2 className="text-font-color 
+              md:text-7xl  
+              md:leading-[15px]
+
+              text-[40px]
+              
+               ">nothing but 
               </h2>
 
-              <h1 className="text-secondary text-9xl">the best</h1>
-            </div>
-            <h2 className="text-secondary font-nautigal text-6xl">Frank Sinatra</h2>
+              <h1 className="text-secondary 
+              md:text-9xl 
+              md:leading-[45px]
+              md:ml-0
 
+              text-[64px]
+              ml-40
+          
+              
+              ">the best</h1>
+            </div>
+
+           
+            <h2 className=" text-secondary font-nautigal 
+              md:text-6xl
+              text-5xl"> - Frank Sinatra</h2>
+            
          </div>
 
          {/* container for texts*/ }
 
         </div>
 
-        <div className="w-1/2  bg-secondary h-screen flex justify-end items-end  ">
+        <div className="w-1/2 hidden sm:block  bg-secondary h-screen flex justify-end items-end  ">
         <img className='h-full ' src={Frank} alt="Frank" />
         </div>
-    </div>
+    </section>
   )
 }
+
+
--- SinatraWebsite/src/main.tsx
+++ SinatraWebsite/src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Hero from './components/Hero'
+import Navbar from './components/Navbar'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Navbar/>
     <Hero/>
   </React.StrictMode>,
 )
